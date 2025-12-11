5cfbc0c6c32c7783ddb34b59f681cb733593db0b
KauaneMineiroCoimbra <kauanemineirok@gmail.com>
2025-12-11 15:34:03 -0300

    Aula_01

---
+++ Impressao_Basica_02.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kauane Mineiro 
+ */
+//Fazer impressão Basica de Desenho Simples
+public class Impressao_Basica_02 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner (System.in);
+        System.out.println("+-----+"); 
+        System.out.println("´---`");
+    }
+   
+}
