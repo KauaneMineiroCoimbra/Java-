ec9a31c0ab83d0a6868744f05f13bf35f1f87376
KauaneMineiroCoimbra <kauanemineirok@gmail.com>
2025-12-11 16:00:24 -0300

    Estrutura_de_Repetição_Aula06

---
+++ Estrutura_de_Repetição_01.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @authora Kauane Mineiro 
+ */
+// Aula 06- Exercicio 01
+// Exercicio 01- contar os pares 
+public class Estrutura_de_Repetição_01 {
+    public static void main(String[] args) {
+       for (int i=0; i<=20; i+=2) {
+           System.out.println("i"); 
+    }
+
+        
+    }
+  
+}
---
+++ Estrutura_de_Repetição_02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @authora Kauane Mineiro 
+ */
+//Aula 06 - estrutura de repetição
+//Exercicio 02-  de 10 ate 1 
+public class Estrutura_de_Repetição_02 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i>=1) {
+        System.out.println("i");
+        i--;
+        }
+    }
+   
+}
