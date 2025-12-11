3ebc3618c53fd3f433d79cfc9cf22dc3552a7700
KauaneMineiroCoimbra <kauanemineirok@gmail.com>
2025-12-11 16:02:35 -0300

    Aula 07- Vetores

---
+++ Vetores_01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @authora Kauane Mineiro 
+ */
+//Aula 07 - Vetores
+//Exercicio 01- vetor 5 inteiros e imprimir
+ public class Vetores_01 {
+ public static void main(String[] args) {
+  int[] v = {1,2,3,4,5};
+  for ( int i =0; i <v.length; i++) {
+      System.out.println("i[v]");
+  }
+  
+}   
+}
